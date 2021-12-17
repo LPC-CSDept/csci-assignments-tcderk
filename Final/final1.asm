733ce72623329e072eac2e8ee907d06b0519973c
tcderk <derekt2288@gmail.com>
2021-12-16 16:38:49 -0800

    finished prototype

--- Final/final1.asm
+++ Final/final1.asm
@@ -10,9 +10,10 @@
     .globl main
 
 main:
-    li  $s0, 10                     # Constant. Shifts result decimal places
-    lui $s1, 0xFFFF                 # For MMI/O registers. Address of receiver control
-    and $s2, $s2, $zero             # Result register
+    li      $s0, 10                 # Constant. Shifts result decimal places
+    lui     $s1, 0xFFFF             # For MMI/O registers. Address of receiver control
+    and     $s2, $s2, $zero         # Result register
+    addi    $s3, $s3, 3             # Counter register
 
 loop:
     lw      $t0, 0($s1)             # Read receiver control
@@ -22,3 +23,11 @@ loop:
     lw      $t1, 4($s1)             # Read receiver data
     mul     $s2, $s2, $s0           # Shift result one decimal left
     add     $s2, $s2, $t1           # Add result decimal to ones place of result
+    addi    $s3, $s3, -1            # Decrement counter
+    bgtz    $s3, loop               # Terminate loop after 3 iterations
+
+    or      $a0, $s2, $zero         # Copy result into $a0
+    li      $v0, 1                  # Print integer
+    syscall
+    li      $v0, 10
+    syscall
