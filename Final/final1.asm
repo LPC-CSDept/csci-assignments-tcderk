92ca53ed3de9531c206716512ea2aa7a2461d3bd
tcderk <derekt2288@gmail.com>
2021-12-16 16:27:56 -0800

    calculations

--- Final/final1.asm
+++ Final/final1.asm
@@ -12,6 +12,7 @@
 main:
     li  $s0, 10                     # Constant. Shifts result decimal places
     lui $s1, 0xFFFF                 # For MMI/O registers. Address of receiver control
+    and $s2, $s2, $zero             # Result register
 
 loop:
     lw      $t0, 0($s1)             # Read receiver control
@@ -19,3 +20,5 @@ loop:
     beq     $t0, $zero, loop        # Loop if not ready
     nop
     lw      $t1, 4($s1)             # Read receiver data
+    mul     $s2, $s2, $s0           # Shift result one decimal left
+    add     $s2, $s2, $t1           # Add result decimal to ones place of result
