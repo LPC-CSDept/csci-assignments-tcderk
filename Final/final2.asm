dc52b1654bcdf768ebef7f00f8e18939e516ed2f
tcderk <derekt2288@gmail.com>
2021-12-16 17:39:40 -0800

    get exception code

--- Final/final2.asm
+++ Final/final2.asm
@@ -6,6 +6,10 @@
 # Prints user's input until 'q' is entered
 #
 
+    .kdata
+s1: .word   10
+s2: .word   11
+
     .text
     .globl main
 
@@ -16,3 +20,15 @@ main:
     li          $s0, 0xFFFF0000     # Constant. Address of receiver control
     li          $a0, 2              # To enable keyboard interrupts
     sw          $a0, 0($s0)         # Write to receiver control
+
+loop:
+    j           loop                # Infinite loop
+
+    .ktext  0x80000180              # Beginnning of kernel text
+    sw          $v0, s1
+    sw          $a0, s2             # Store $v0 and $a0
+
+    mfc0        $k0, $13            # Read cause register
+    srl         $a0, $k0, 2         # Read exception code
+    andi        $a0, $a0, 0x1f      # Only need exception code (5 bits)
+    
