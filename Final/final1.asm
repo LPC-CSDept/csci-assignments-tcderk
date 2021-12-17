4398933971c011a38b4eadd27c682f90d5ef2a9f
tcderk <derekt2288@gmail.com>
2021-12-16 16:20:08 -0800

    implemented input

--- Final/final1.asm
+++ Final/final1.asm
@@ -10,8 +10,12 @@
     .globl main
 
 main:
-    li  $s0, 10         # Constant. Shifts result decimal places
-    lui $s1, 0xFFFF    # For MMI/O registers. Address of receiver control
+    li  $s0, 10                     # Constant. Shifts result decimal places
+    lui $s1, 0xFFFF                 # For MMI/O registers. Address of receiver control
 
 loop:
-    
+    lw      $t0, 0($s1)             # Read receiver control
+    andi    $t0, $t0, 1             # Check ready bit
+    beq     $t0, $zero, loop        # Loop if not ready
+    nop
+    lw      $t1, 4($s1)             # Read receiver data
